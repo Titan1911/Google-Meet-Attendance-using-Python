c2e473e08844409538984ffa0ae196712481ea24
19-ade <anwesan4github@gmail.com>
2021-07-22 09:03:44 -0700

    Downloads lib automatically

---
+++ automated_download.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+import sys
+
+
+def install(package):
+    os.system("pip install " + str(package))
+    reqs = subprocess.check_output([sys.executable, "-m", "pip", "show", str(package)])
+
+    print(str(reqs) + "\n")
+    print("Installed " + package.upper() + "\n")
+
+
+install("openpyxl")
+install("selenium")
+
